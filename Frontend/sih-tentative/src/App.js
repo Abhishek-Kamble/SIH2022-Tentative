336faf1a47a482e62ac26f5313573d4500740b7f
Vai69 <disalevaishnavi2001@gmail.com>
2022-08-15 20:30:28 +0530

    routing updated

--- Frontend/sih-tentative/src/App.js
+++ Frontend/sih-tentative/src/App.js
@@ -1,10 +1,18 @@
 import "./App.css";
+import {  BrowserRouter,Route, Switch } from "react-router-dom";
+import LandingPage from "./components/LandingPage";
+import Navbar from "./components/Navbar"
 
 function App() {
   return (
-    <div className="App">
-      <h1>Homepage</h1>
-    </div>
+    <BrowserRouter>
+      <Navbar></Navbar>
+          <div className="content">
+                    <Switch>
+                        <Route exact path="/" component={LandingPage}/>
+                    </Switch>
+          </div>
+    </BrowserRouter>
   );
 }
 export default App;
