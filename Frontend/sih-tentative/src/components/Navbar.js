180b86600d9c40a39ce123791a08d7d4c9bc13c9
Vai69 <disalevaishnavi2001@gmail.com>
2022-08-16 15:01:32 +0530

    navbar added

--- Frontend/sih-tentative/src/components/Navbar.js
+++ Frontend/sih-tentative/src/components/Navbar.js
@@ -1,13 +1,56 @@
-import  { NavLink } from "react-router-dom";
+import React, { useState } from "react";
+import "../CSS/Navbar.css";
+import { GiHamburgerMenu } from "react-icons/gi";
 
-const Navbar=()=> {
-    return (
-        <div className="navbar">
-            <ul>
-                <li><NavLink exact to="/">Home</NavLink></li>
-            </ul>
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  const [showMediaIcons, setShowMediaIcons] = useState(false);
+  return (
+    <>
+      <nav className="main-nav">
+        {/* 1st logo part  */}
+        <div className="logo">
+          <h2>
+              EGOV
+          </h2>
         </div>
-    )
-}
+
+        {/* 2nd menu part  */}
+        <div
+          className={
+            showMediaIcons ? "menu-link mobile-menu-link" : "menu-link"
+          }>
+          <ul>
+            <li>
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/about">about</NavLink>
+            </li>
+            <li>
+              <NavLink to="/service">services</NavLink>
+            </li>
+            <li>
+              <NavLink to="/contact">contact</NavLink>
+            </li>
+          </ul>
+        </div>
+
+        {/* 3rd social media links */}
+        <div className="social-media">
+           
+
+          {/* hamburget menu start  */}
+          <div className="hamburger-menu">
+            <a href="#" onClick={() => setShowMediaIcons(!showMediaIcons)}>
+              <GiHamburgerMenu />
+            </a>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
 
 export default Navbar;
