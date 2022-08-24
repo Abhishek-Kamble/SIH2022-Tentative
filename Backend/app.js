94e82f6cfe013f0b21eb391d53c5bcfb06dc1864
Abhishek Kamble <abhishek.kamble@walchandsangli.ac.in>
2022-08-24 14:45:23 +0530

    Change port

--- Backend/app.js
+++ Backend/app.js
@@ -36,10 +36,10 @@ expressApp.use('/p',Password);
 
 let port = process.env.PORT;
 if (port == null || port == "") {
-    port = 8000;
+    port = 5000;
 }
 
 
 expressApp.listen(port,function(){
-    console.log("server has started on port 8000");
+    console.log("server has started on port 5000");
 })
