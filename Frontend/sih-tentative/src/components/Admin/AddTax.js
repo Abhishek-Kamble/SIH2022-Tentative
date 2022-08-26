dfc2683b6124674cfc37f10b0d460b587c9063b2
Vai69 <disalevaishnavi2001@gmail.com>
2022-08-26 06:42:08 +0530

    dashboard

--- Frontend/sih-tentative/src/components/Admin/AddTax.js
+++ Frontend/sih-tentative/src/components/Admin/AddTax.js
@@ -1,9 +1,142 @@
-import React from 'react'
+// import { useEffect, useState } from "react";
+// import FormInput from "./views/FormInput";
+// import { Message, Button } from "semantic-ui-react";
+// import axiosconfig, { setToken } from "../../config";
+// import { ToastContainer, toast } from "react-toastify";
+// import "react-toastify/dist/ReactToastify.css";
+// import { notify } from "../toast";
 
-export default function AddTax(){
+// const AddTax = () => {
+//     useEffect(() => {
+//         require("../../CSS/StaffRegistration.css")
+//     })
 
-    return (
-      <div>AddTax</div>
-    )
+//     const [values, setValues] = useState({
+//         fname: "",
+//         lname: "",
+//         email: "",
+//         mobile_number: "",
+//         password: "",
+//         confirmPassword: "",
+//     });
 
-}
+//     const inputs = [
+//         {
+//             id: 1,
+//             name: "Tax",
+//             type: "text",
+//             placeholder: "Lastname",
+//             label: "Lastname",
+//             required: true,
+//         },
+//         {
+//             id: 2,
+//             name: "email",
+//             type: "email",
+//             placeholder: "Email",
+//             errorMessage: "It should be a valid email address!",
+//             label: "Email",
+//             required: true,
+//         },
+//     ];
+//     const chaeckData = (obj) => {
+//         console.log(obj);
+//         const { email, password, fname, lname, confirmPassword, mobile_number } = obj;
+//         if (fname.length == 0) {
+//             notify('Please enter first name', 'error');
+//             return;
+//         }
+//         if (lname.length == 0) {
+//             notify('Please enter last name', 'error');
+//             return;
+//         }
+//         if (email.length == 0) {
+//             notify('Please enter a valid email', 'error');
+//             return;
+//         }
+//         if (mobile_number.length != 10) {
+//             console.log("invalid mobile");
+//             notify('Please enter a valid mobile number', 'error');
+//             return;
+//         }
+//         if (password.length == 0) {
+//             notify('Please enter a valid password', 'error');
+//             return;
+//         }
+//         if (confirmPassword != password) {
+//             notify('Password don\'t match', 'error');
+//             return;
+//         }
+
+
+//         var link = '/staffregistor';
+//         setToken(localStorage.getItem('token'));
+//         const api = axiosconfig
+//             .post(link, obj)
+//             .then((response) => response.data)
+//             .then(async (data) => {
+//                 console.log(data)
+//                 if (data.found) {
+
+//                     notify("You have registered successfully", "success")
+//                     window.location.href = "http://localhost:3000/staffInfo";
+
+//                 } else {
+//                     notify(data.message, "error")
+//                 }
+//             });
+//         toast.promise(api, {
+//             pending: "Loading your data...",
+//             success: false,
+//             error: "Something went wrong!",
+//         });
+//     };
+//     const handleSubmit = (e) => {
+//         e.preventDefault();
+//         chaeckData(values);
+//     };
+
+//     const onChange = (e) => {
+//         setValues({ ...values, [e.target.name]: e.target.value });
+//     };
+
+//     const handleGoHome = () => {
+//         console.log("In handle");
+//         window.location.href = "http://localhost:3000/";
+//     }
+
+//     if (localStorage.getItem('role') == '1') {
+//         return (
+//             <div className="app">
+//                 <form onSubmit={handleSubmit}>
+//                     <h1>Add tax</h1>
+//                     <br></br><br></br>
+//                     {inputs.map((input) => (
+//                         <FormInput
+//                             key={input.id}
+//                             {...input}
+//                             value={values[input.name]}
+//                             onChange={onChange}
+//                         />
+//                     ))}
+//                     <div className="btnn">
+//                         <button onClick={handleSubmit} className="btn_submit">Submit</button>
+//                     </div>
+//                 </form>
+//                 <ToastContainer />
+//             </div>
+//         );
+//     }
+//     else {
+//         return (
+//             <Message floating style={{ padding: '60px' }}>
+//                 <h2>You are not authorised as Admin. Please login again!</h2>
+//                 <div>
+//                     <Button color='primary' onClick={handleGoHome}>Go to Home</Button>
+//                 </div>
+//             </Message>
+//         )
+//     }
+// };
+
+// export default AddTax;
--- Frontend/sih-tentative/src/components/Admin/Dashboard.js
+++ Frontend/sih-tentative/src/components/Admin/Dashboard.js
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useEffect} from 'react'
 import { Link } from 'react-router-dom';
 import { Button,Message } from 'semantic-ui-react';
 
 export default function AdminDashboard() {
+    useEffect(() => {
+        require("../../CSS/home.css")
+    })
     const handleGoHome = () => {
         console.log("In handle");
         window.location.href = "http://localhost:3000/";
@@ -11,12 +14,68 @@ export default function AdminDashboard() {
     if (localStorage.getItem('role') == '1') {
         return (
             <>
-                <div style={{padding:"30px"}}>
-                    <Button style={{ backgroundColor: 'blue' }}><Link style={{ color: 'white' }} to="/addZone">Add Zone</Link></Button>
-                </div>
-                <div style={{ padding: "30px" }}>
-                    <Button style={{ backgroundColor: 'blue' }}><Link style={{ color: 'white' }} to="/taxRate">Tax Rates</Link></Button>
-                </div>
+                <div className="LandingPage">
+                    <article className="bg__Head">
+                        <div className="container2">
+                            <div className="titleBox2">
+                                <h1 className="entry-title">Property Tax Management System</h1>
+                            </div>
+                        </div>
+                    </article>
+                    <div id="latest_news">
+                        <div className="container1">
+                            <div className="marquee-sibling">
+                                LATEST NEWS
+                            </div>
+                            <div className="marquee">
+                                <ul className="marquee-content-items">
+                                    <li>Get Discount on your Property Tax - Pay Before 3rd June</li>
+                                    <li>You Can Pay your Tax Online / Bank Collection Centres / Cheque / Cash.</li>
+                                    <li>2% Penalty Charged on Arrears on 1st day of each month.</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="container3">
+                        <h1>Services</h1>
+                        <div class="row1">
+                            <div class="service">
+                                {/* <i class="fas fa-laptop-code"></i> */}
+                                <img src="/images2/assessment.png" />
+                                <h2>Assessment Of Property</h2>
+                                <p>
+                                    To assess Property tax on Open land and buildings within the jurisdiction of city
+                                </p>
+                            </div>
+
+                            <div class="service">
+                                <img src="/images2/graph-3.png" />
+                                <h2>Collection</h2>
+                                <p>
+                                    To collect Property Tax.
+                                </p>
+                            </div>
+                        </div>
+                        <div class="row1">
+                            <div class="service">
+                                <img src="/images2/PropertyDetails.png" />
+                                <h2>CASHLESS PAYMENT OPTIONS</h2>
+                                <p>
+                                    Convenient ways to pay your tax
+                                </p>
+                            </div>
+
+                            <div class="service">
+                                <img src="/images2/cashless.jpg" />
+                                <h2>PROPERTY TAX RECEIPT </h2>
+                                <p>
+                                    To get Property Tax Receipt.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    </div>
             </>
         )
     }
